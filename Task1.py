a28bead06b318a2cc98c4c41d4b45b3366d1a80c
ARJUN0262 <164337232+ARJUN0262@users.noreply.github.com>
2025-07-13 19:36:15 +0530

    Delete Task1.py

--- Task1.py
+++
@@ -1,9 +0,0 @@
-#Inputs in the variables num1 & num2
-num1= int(input('Enter the first number : ' ))
-num2 = int(input('Enter the second number : '))
-
-#Print the results with Addition,Subtraction,Multiplication & Division
-print('Addition : ' , num1+num2)
-print('Subtraction : ' , num1-num2)
-print('Multiplication : ',num1*num2)
-print('Division : ',num1/num2)
